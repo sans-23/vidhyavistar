32e5412400019427e4fa9b153fda4beec30d3181
Sanskar Jain <sans@192.168.1.2>
2024-11-19 21:46:51 +0530

    User Entity and corresponding jpa repo created

---
+++ night-watch/src/main/java/com/vidhyavistaar/night_watch/entity/AuthRequest.java
@@ -0,0 +1,13 @@
+package com.vidhyavistaar.night_watch.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    private String username;
+    private String password;
+}
