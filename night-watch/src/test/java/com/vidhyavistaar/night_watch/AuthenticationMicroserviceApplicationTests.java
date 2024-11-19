c804f59b202f5c445055570f9ccda09bf963cc0f
Sanskar Jain <sans@192.168.1.2>
2024-11-19 21:16:13 +0530

    Authentication microservice initialised

---
+++ night-watch/src/test/java/com/vidhyavistaar/night_watch/AuthenticationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhyavistaar.night_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
